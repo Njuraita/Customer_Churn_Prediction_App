8cc6b0ddbbc501408146ecac97b3ce8ace6b464c
Sharon Njuraita <njerisharon06@gmail.com>
2024-11-23 18:28:15 +0300

    first commit

---
+++ Home.py
@@ -0,0 +1,99 @@
+# Import necessary libraries
+import streamlit as st
+
+# Configure the page
+st.set_page_config(
+    page_title='Customer Churn Prediction App',
+    page_icon='👨‍💻',
+    layout='wide',
+    initial_sidebar_state='auto'
+)
+
+# Add custom CSS to adjust the width of the sidebar
+st.markdown("""
+    <style> 
+        section[data-testid="stSidebar"] {
+            width: 200px !important;
+        }
+    </style> 
+""", unsafe_allow_html=True)
+
+def main():
+    st.header('Customer Churn Prediction App')
+
+    st.write(
+        """
+        Welcome to the **Customer Churn Prediction App**! Our app is designed to help businesses understand and predict customer churn using advanced data analysis and machine learning techniques.
+        """
+    )
+
+    cols = st.columns(2)
+
+    # Churn Prediction Status
+    with cols[0]:
+        st.subheader('About the App')
+        st.write(
+            """
+            This application leverages historical data to predict the likelihood of customer churn. By analyzing customer demographics, subscription details, and account information, the app helps businesses:
+            
+            * Identify customers at risk of churning
+            * Understand factors influencing churn
+            * Implement strategies to retain valuable customers
+            
+            With a user-friendly interface and powerful predictive models, our app provides actionable insights to enhance customer retention strategies.
+            """
+        )
+
+    # Application Features
+    with cols[0]:
+        st.subheader('Key Features')
+        st.markdown("""
+            * **Data View**: Explore and analyze customer data to gain insights into churn patterns.
+            * **Dashboard**: Visualize key metrics and trends through interactive charts and graphs.
+            * **Predict**: Use our predictive models to estimate the likelihood of customer churn based on various factors.
+            * **History**: Review past predictions and track changes over time.
+            """)
+
+    # Key Advantages
+    with cols[0]:
+        st.subheader('Why Use This App?')
+        st.markdown("""
+            * **Accurate Predictions**: Benefit from state-of-the-art machine learning models for reliable churn forecasts.
+            * **Intuitive Interface**: Navigate through a user-friendly interface designed for ease of use.
+            * **Actionable Insights**: Gain insights into customer behavior and retention strategies.
+            * **Continuous Improvement**: Regular updates and enhancements to keep up with the latest trends and technologies.
+            """)
+
+    # How to Run the App
+    with cols[1]:
+        st.subheader('How to Get Started')
+        st.write("Follow these steps to run the Customer Churn Prediction App:")
+        st.code("""
+            # Activate your virtual environment
+            venv/Scripts/activate
+
+            # Run the Streamlit app
+            streamlit run app.py
+            """, language="python")
+
+    # Machine Learning Integration
+    with cols[1]:
+        st.subheader('Machine Learning Models')
+        st.write(
+            """
+            Our app integrates advanced machine learning models, including Gradient Boosting and Support Vector Machines (SVM). These models are trained on historical data to deliver accurate predictions and help businesses make informed decisions.
+            """)
+
+    # Need Assistance
+    with cols[1]:
+        st.subheader('Need Help?')
+        st.write(
+            """
+            If you encounter any issues or have questions, please don't hesitate to reach out:
+            - **Email**: just.hanson1@gmail.com
+            - **GitHub**: [GitHub Repository](https://github.com/JusticeHanson/GUI-Machine-Learning-App.git)
+            - **LinkedIn**: [Connect on LinkedIn](http://www.linkedin.com/in/justice-hanson)
+            """)
+
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,89 @@
+# Churn Prediction App
+
+## 📖 Table of Contents
+- [Churn Prediction App](#churn-prediction-app)
+- [🛠 Built With](#-built-with)
+  - [Tech Stack](#tech-stack)
+- [Application Features](#application-features)
+- [💻 Getting Started](#-getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Setup](#setup)
+  - [Install](#install)
+- [Usage](#usage)
+- [👥 Authors](#-authors)
+- [🔭 Future Features](#-future-features)
+- [🤝 Contributing](#-contributing)
+- [⭐️ Show Your Support](#-show-your-support)
+- [🙏 Acknowledgments](#-acknowledgments)
+- [📝 License](#-license)
+
+## Churn Prediction App
+
+Churn Prediction App provides a platform for predicting the churn status of customers by leveraging historical data encompassing customer demographics, subscription details, account information, and their corresponding churn status.
+
+## 🛠 Built With
+
+### Tech Stack
+- **GUI:** Streamlit
+- **Database:** CSV file for history data
+- **Language:** Python
+- **Model:** Scikit-learn models (Gradient Boosting, Support Vector)
+
+## Application Features
+- Data application with visualizations for exploratory data and KPIs
+- Predictions page to predict by specifying the desired model
+- Saved predictions for future analysis with access to prediction input history
+
+## 💻 Getting Started
+
+To get a local copy up and running, follow these steps.
+
+### Prerequisites
+
+In order to run this project you need:
+- Python
+- Streamlit
+
+# Setup
+
+Clone this repository to your desired folder:
+- cd desired-folder
+git clone https://github.com/JusticeHanson/GUI-Machine-Learning-App
+
+# Change into the cloned repository:
+
+- cd ML-Embedding-in-GUI
+
+# Create a virtual environment:
+
+- python -m venv venv
+Activate the virtual environment:
+
+# On Windows:
+- venv\Scripts\activate
+
+# On macOS/Linux:
+- source venv/bin/activate
+
+# Install
+
+Install the required packages needed for this project which can be found in the requirements.txt file:
+
+
+pip install -r requirements.txt
+
+# Usage
+
+Run the app by executing the command below:
+
+streamlit run 00_🏠_Home.py
+
+# 👥 Authors
+
+Justice Hanson
+
+🙏 Acknowledgments
+I would like to thank all the freely available resources made available online.
+
+📝 License
+This project is licensed under the MIT License - see the LICENSE file for details.
